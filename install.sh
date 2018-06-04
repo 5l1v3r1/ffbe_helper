912a0e40560315c718f41a4639e1d2f425964fc2
root <root@p200300C07726B3BC020C29FFFED98836.Speedport_W_921V_1_44_000>
2018-06-04 13:48:46 -0700

    update

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 sudo apt-get update && sudo apt-get install git squid -y
-cd /tmp/ && git clone https://github.com/Mila432/ffbe_helper.git && cd ffbe_helper
-sudo cp squid.conf /etc/squid/squid.conf
+cd /tmp/ && git clone https://github.com/Mila432/ffbe_helper.git && cd /tmp/ffbe_helper
+sudo cp /tmp/ffbe_helper/squid.conf /etc/squid/squid.conf
 sudo service squid restart
 echo "setup done"
